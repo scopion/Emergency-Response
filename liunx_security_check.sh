efbd5f792dd3b4538f6ee8359d21aacdaa92d6d0
root <qinnuo1101@gmail.com>
2017-09-19 16:39:39 +0800

    update

--- liunx_security_check.sh
+++ liunx_security_check.sh
@@ -16,7 +16,6 @@
 #else
 #    WEBCHK=0
 #fi
-#echo -e "\033[33m#### 0x0. Check rootkit  \033[0m"
 if [ -f /etc/redhat-release ]; then
 #	yum -y update
 	yum -y install epel-release
@@ -26,20 +25,6 @@ else
 	apt-get -y update
 	apt-get -y install wget git build-essential rkhunter unhide clamav
 fi
-echo -e "\033[33m|--> Check rootkit\033[0m"
-rkhunter -c --sk > /tmp/rkhunter.log
-grep 'Warning' /tmp/rkhunter.log
-echo ""
-echo 'check hiden progress'
-nohup unhide sys >>/tmp/unhide.log
-nohup unhide brute >>/tmp/unhide.log
-nohup unhide proc >>/tmp/unhide.log
-nohup unhide procall >>/tmp/unhide.log
-nohup unhide procfs >>/tmp/unhide.log
-nohup unhide quick >>/tmp/unhide.log
-nohup unhide reverse >>/tmp/unhide.log
-nohup unhide-tcp >>/tmp/unhide.log
-cat check/unhide.log
 
 echo -e "\033[33m#### 0x1. Check system source use \033[0m"
 echo -e "\033[33m|--> Check OS Version \033[0m"
@@ -70,6 +55,7 @@ echo -e "\033[33m#### 0x3. Check service and task info \033[0m"
 echo -e "\033[33m|--> Check start chkconfig service \033[0m"
 chkconfig --list |grep -E '2:on|3:on' | awk '{printf"%s ",$1}'
 systemctl list-unit-files |grep -E '2:on|3:on' | awk '{printf"%s ",$1}'
+sysv-rc-conf --list
 echo ""
 echo -e "\033[33m|--> Check start rc.local \033[0m"
 grep -vE '^$|^#' /etc/rc.local
@@ -78,7 +64,7 @@ crontab -l
 ls -la /var/spool/cron/ >> /tmp/checkcrontab.log
 ls -la /etc/cron* >> /tmp/checkcrontab.log
 echo ""
-echo 'check remote service'
+echo -e "\033[33m|--> Check remote service \033[0m"
 cat /etc/inetd.conf | grep -v "^#"
 echo -e "\033[33m#### 0x4. Check process info \033[0m"
 echo -e "\033[33m|--> Check ps result \033[0m"
@@ -107,6 +93,7 @@ echo -e "\033[33m|--> Check network service info\033[0m"
 netstat -nap | grep LISTEN
 echo -e "\033[33m|--> Check process <-> port info\033[0m"
 lsof -i > /tmp/checklsof
+#cat /tmp/checklsof
 echo ""
 
 echo -e "\033[33m#### 0x6. Check file and module info \033[0m"
@@ -132,6 +119,7 @@ echo -e "\033[33m|--> Check ssh login success history\033[0m"
 last -n 30
 echo -e "\033[33m|--> Check ssh login fail history\033[0m"
 grep -ri "ail" /var/log/secure* | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'| sort -n | uniq | awk '{printf"%s ",$1}'
+last -f  /var/log/btmp -20
 echo ""
 echo -e "\033[33m|--> Check bash history size\033[0m"
 echo $HISTSIZE
@@ -146,11 +134,26 @@ echo -e "\033[33m|--> Check webshell \033[0m"
 find / -type f -name '*.php' | xargs egrep '(phpspy|c99sh|milw0rm|eval\(gunerpress|eval\(base64_decode|spider_bc|@$)' | awk -F : '{print $1}' >> /tmp/checkweb.log
 find / -type f -name "*.jsp" |xargs egrep 'exec|request.getParameter' >> /tmp/checkweb.log
 find / -type f -name "*.asp*" |xargs egrep 'eval|execute|Request|VBScript'>> /tmp/checkweb.log
-grep -i 'select%20|sqlmap|script|phpinfo()|upload|cat' $LOGDIR/*log  | grep 500 | grep -i \.php >> /tmp/checkweb.log
-find / -type f -name '*tunnel*'>> /tmp/checkweb.log
+#grep -i 'select%20|sqlmap|script|phpinfo()|upload|cat' $LOGDIR/*log  | grep 500 | grep -i \.php >> /tmp/checkweb.log
 find / -type f -name '*struts*'>> /tmp/checkweb.log
-cat /tmp/checkweb.log 
+#cat /tmp/checkweb.log 
 echo ""
+
+echo -e "\033[33m#### 0x9. Check rootkit \033[0m"
+echo -e "\033[33m|--> Check sysfile\033[0m"
+rkhunter -c --sk > /tmp/rkhunter.log
+grep 'Warning' /tmp/rkhunter.log
+echo ""
+echo -e "\033[33m|--> Check hiden progress\033[0m"
+nohup unhide sys >>/tmp/unhide.log
+nohup unhide brute >>/tmp/unhide.log
+nohup unhide proc >>/tmp/unhide.log
+nohup unhide procall >>/tmp/unhide.log
+nohup unhide procfs >>/tmp/unhide.log
+nohup unhide quick >>/tmp/unhide.log
+nohup unhide reverse >>/tmp/unhide.log
+nohup unhide-tcp >>/tmp/unhide.log
+#cat /tmp/unhide.log
 
 echo 'check backdoors'
 find / -name “.rhosts” -print 
@@ -162,5 +165,7 @@ ls -la /var/log/mail/
 
 #echo -e "\033[33m|--> Check av\033[0m"
 #freshclam  >/dev/null 2>&1 
-#clamscan -r --bell -i / > /tmp/clamav.log 2>&1
-#tail -10  /tmp/clamav.log
+#clamscan -r --bell -i / > /tmp/checkclamav.log 2>&1
+#tail -10  /tmp/checkclamav.log
+
+echo '/tmp/checkcrontab.log /tmp/checkfilechange /tmp/checklsof /tmp/unhide.log /tmp/checksystool.log /tmp/rkhunter.log /tmp/checkweb.log'
